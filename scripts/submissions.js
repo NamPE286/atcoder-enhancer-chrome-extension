29892847e16907449b61a8c4ccf63f595a561d35
NamPE286 <nambuihung654@gmail.com>
2024-07-14 19:06:37 +0700

    update

--- scripts/submissions.js
+++ scripts/submissions.js
@@ -2,16 +2,23 @@ const contest = window.location.pathname.split('/')[2];
 const problem = document.getElementsByTagName('td')[1].innerText[0];
 
 let dialog = document.createElement('dialog');
-let copyBtnHTMLString =
-    '<div class="div-btn-copy"><span class="btn-copy btn-pre" tabindex="0" data-toggle="tooltip" data-trigger="manual" title="" data-target="pre-sample4" data-original-title="Copied!">Copy</span></div>';
-dialog.innerHTML = `<button id='testcase-dialog-close-btn'>x</button><br><div id='testcase-dialog-detail'></div>`;
+let copyBtnHTMLString =`
+    <div class="div-btn-copy">
+        <span class="btn-copy btn-pre" tabindex="0" data-toggle="tooltip" data-trigger="manual" title="" data-target="pre-sample4" data-original-title="Copied!">Copy</span>
+    </div>
+`;
+
+dialog.innerHTML = `
+    <button id='testcase-dialog-close-btn'>x</button><br>
+    <div id='testcase-dialog-detail'></div>
+`
 
 document.body.appendChild(dialog);
-document.getElementById('testcase-dialog-detail').style.width = '1000px'
-document.getElementById('testcase-dialog-detail').style.maxWidth = '100%'
-document.getElementById('testcase-dialog-close-btn').style.float = 'right'
-document.getElementById('testcase-dialog-close-btn').style.backgroundColor = 'transparent'
-document.getElementById('testcase-dialog-close-btn').style.border = 'none'
+document.getElementById('testcase-dialog-detail').style.width = '1000px';
+document.getElementById('testcase-dialog-detail').style.maxWidth = '100%';
+document.getElementById('testcase-dialog-close-btn').style.float = 'right';
+document.getElementById('testcase-dialog-close-btn').style.backgroundColor = 'transparent';
+document.getElementById('testcase-dialog-close-btn').style.border = 'none';
 document
     .getElementById('testcase-dialog-close-btn')
     .addEventListener('click', () => {
@@ -37,20 +44,25 @@ function extractResultData() {
     return res;
 }
 
-async function showTestcase(caseName) {
+async function showTestcase(item) {
     const dialogDetail = document.getElementById('testcase-dialog-detail');
-
+    dialogDetail.style.textAlign = 'center'
     dialogDetail.innerHTML = 'Loading...';
     dialog.showModal();
 
     try {
         const testData = await (
             await fetch(
-                `https://atcoder-enhancer-api.fly.dev/contest/${contest}/${problem}/testcase/${caseName}`
+                `https://atcoder-enhancer-api.fly.dev/contest/${contest}/${problem}/testcase/${item.caseName}`
             )
         ).json();
 
-        dialogDetail.innerHTML = `<b>Input:</b>${copyBtnHTMLString}<pre id="pre-sample6">${testData.in}</pre><b>Output:</b>${copyBtnHTMLString}<pre id="pre-sample6">${testData.out}</pre>`;
+        dialogDetail.style.textAlign = 'left'
+        dialogDetail.innerHTML = `
+            <b>Status:</b> ${item.status}; <b>Exec Time:</b> ${item.execTime}ms; <b>Memory:</b> ${item.memory}KB<br>
+            <b>Input:</b>${copyBtnHTMLString}<pre id="pre-sample6">${testData.in}</pre>
+            <b>Output:</b>${copyBtnHTMLString}<pre id="pre-sample6">${testData.out}</pre>
+        `;
     } catch {
         dialogDetail.innerHTML = 'Not available';
     }
@@ -62,7 +74,7 @@ if (window.location.pathname.split('/')[4] != 'me') {
     for (const i of results) {
         i.element.style.cursor = 'pointer';
         i.element.onclick = () => {
-            showTestcase(i.caseName);
+            showTestcase(i);
         };
     }
 }
