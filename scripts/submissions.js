30c4f0f5806daca651893bfd02932c1762e6d51b
NamPE286 <nambuihung654@gmail.com>
2024-07-15 06:50:16 +0700

    fix result table detection

--- scripts/submissions.js
+++ scripts/submissions.js
@@ -2,7 +2,7 @@ const contest = window.location.pathname.split('/')[2];
 const problem = document.getElementsByTagName('td')[1].innerText[0];
 
 let dialog = document.createElement('dialog');
-let copyBtnHTMLString =`
+let copyBtnHTMLString = `
     <div class="div-btn-copy">
         <span class="btn-copy btn-pre" tabindex="0" data-toggle="tooltip" data-trigger="manual" title="" data-target="pre-sample4" data-original-title="Copied!">Copy</span>
     </div>
@@ -11,13 +11,14 @@ let copyBtnHTMLString =`
 dialog.innerHTML = `
     <button id='testcase-dialog-close-btn'>x</button><br>
     <div id='testcase-dialog-detail'></div>
-`
+`;
 
 document.body.appendChild(dialog);
 document.getElementById('testcase-dialog-detail').style.width = '1000px';
 document.getElementById('testcase-dialog-detail').style.maxWidth = '100%';
 document.getElementById('testcase-dialog-close-btn').style.float = 'right';
-document.getElementById('testcase-dialog-close-btn').style.backgroundColor = 'transparent';
+document.getElementById('testcase-dialog-close-btn').style.backgroundColor =
+    'transparent';
 document.getElementById('testcase-dialog-close-btn').style.border = 'none';
 document
     .getElementById('testcase-dialog-close-btn')
@@ -26,14 +27,25 @@ document
     });
 
 function extractResultData() {
-    const resultCells = document.getElementsByTagName('tbody')[5].children;
+    let resultRows;
+    const tables = document.getElementsByTagName('tbody');
+
+    for (let i = tables.length - 1; i >= 0; i--) {
+        const table = tables[i];
+
+        if (table.children[0].childElementCount == 4) {
+            resultRows = table.children;
+            break;
+        }
+    }
+
     const res = [];
 
-    for (let i = 0; i < resultCells.length; i++) {
-        const result = resultCells[i].innerText.split('\t');
+    for (let i = 0; i < resultRows.length; i++) {
+        const result = resultRows[i].innerText.split('\t');
 
         res.push({
-            element: resultCells[i],
+            element: resultRows[i],
             caseName: result[0],
             status: result[1],
             execTime: parseInt(result[2].slice(0, -3)),
@@ -46,7 +58,7 @@ function extractResultData() {
 
 async function showTestcase(item) {
     const dialogDetail = document.getElementById('testcase-dialog-detail');
-    dialogDetail.style.textAlign = 'center'
+    dialogDetail.style.textAlign = 'center';
     dialogDetail.innerHTML = 'Loading...';
     dialog.showModal();
 
@@ -57,7 +69,7 @@ async function showTestcase(item) {
             )
         ).json();
 
-        dialogDetail.style.textAlign = 'left'
+        dialogDetail.style.textAlign = 'left';
         dialogDetail.innerHTML = `
             <b>Case Name: </b> ${item.caseName}<br><b>Status:</b> ${item.status}; <b>Exec Time:</b> ${item.execTime}ms; <b>Memory:</b> ${item.memory}KB<br><br>
             <b>Input:</b>${copyBtnHTMLString}<pre id="pre-sample6">${testData.in}</pre>
